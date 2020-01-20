798ed7a8f9f23214552ce1776d2d338ce61db740
haftom11 <git config --global user.email you@example.com>
2020-01-20 10:58:47 -0800

    Changes to be committed:

---
+++ facupdateexam.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+<?php
+
+session_start();
+ require 'dbconfig/config.php';
+
+
+?>
+
+
+
+<html>
+    <head>
+  
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>            
+    <script src="jquery.tabledit.min.js"></script>
+        
+             
+             <link rel="stylesheet" href="style.css">
+          
+             
+    
+         
+             
+             
+             
+        <style>
+
+                   *{
+
+   margin:0px;
+   padding:0px;
+
+
+}
+header {
+
+
+  height: 50px;
+ background:transparent;
+ border-radius:10px;
+   padding:5px;
+   border:1px solid white;
+}
+footer {
+  background-color: none;
+  bottom: 0;
+  height: 50px;
+  background:transparent;
+  float:center;
+}
+
+.sidebar{
+    margin-top:1px;
+    background-color:blue;
+        height:570px;
+        width:200px;
+        float:left;
+        border-radius: 5px;
+
+
+
+}
+.sidebar h2
+{
+
+    color:#fff;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 30px;
+
+
+
+
+}
+.sidebar ul li{
+    margin-top:27px;
+    padding: 1px;
+    border-bottom: 1px solid rgba(0,0,0,0.05);
+    border-top: 1px solid  rgba(255,255,255, 0.05) ;
+
+      font-size: 14px;
+      list-style: none;
+      font-family: sans-serif;
+      font-style: normal;
+      color:red;
+
+
+
+}
+.sidebar ul li a{
+
+    text-decoration: none;
+   color:#bdb8d7;
+   display: block;
+   font-style: normal;
+   font-size: 14px;
+
+}
+.sidebar ul li a.icon {
+    width: 5px;
+
+}
+.sidebar ul li :hover{
+    background:#00cc00;
+    width: full;
+    border-radius: 3px;
+}
+
+.sidebar  ul li #onlink
+{
+background:black;
+color: white;
+
+
+}
+
+
+
+.sidebar  ul li a #onlink:hover
+{
+background:white;
+color:red;
+
+
+}
+
+
+
+.mainc {
+
+background-color: cadetblue;
+
+  height:600px;
+  marign-top:100px;
+  marign-left:0;
+
+
+   border-radius:10px;
+   padding:5px;
+   border:1px solid white;
+}
+#icon1 #icon2 #icon3  #icon4{
+
+width:30px;
+
+}
+#title{
+
+    background-color:white;
+    width: 700px;
+    height:40px;
+    font-size:24px;
+    border-radius:2px;
+    border: 1px solid  white;
+    text-align: left;
+
+
+
+}
+#logoutbn{
+     marign: 0;
+     margin-right:30px;
+     float:right;
+     background-color:red;
+margin-top: 10px;
+ padding:5px;
+ color:white;
+ width:full;
+  font-size: 18px;
+ text-align: center;
+ border-radius:10px;
+ border: 1px solid red;
+
+
+
+
+}
+#copy{
+text-align:center
+
+
+}
+#status{
+    margin-top: 14px;
+    margin-right: 40px;
+    margin-bottom: 50px;
+    width:full;
+    font-size: 14px;
+     font-family: sans-serif ;
+    float:right
+}
+#log{
+    color:red;
+
+
+
+
+}
+
+
+
+#avator{
+
+    float:left;
+    width:34px;
+
+}
+
+.tabcon{
+
+    width: 1479px;
+    height: 600px;
+
+    background-color:white;
+    padding :5px;
+    border-radius: 5px;
+
+
+
+
+}
+
+.table  thead tr {
+
+  background:#009879;
+  text-align:left;
+  font-weight:bold;
+  color:#ffffff;
+}
+.table th,
+.table td {
+
+  padding :12px 25px;
+}
+
+.table tbody tr
+{ border-bottom: 1px solid #dddddd;}
+.table tbody tr:last-of-type
+{
+ border-bottom: 2px solid #009879;
+
+
+}
+h2{
+
+text-align: center;
+color: green;
+font-family: monospace;
+margin-bottom: 35px;
+
+
+}
+
+
+
+        </style>
+
+    </head>
+
+    <body>
+
+        <header>
+
+        <form  method=post>
+            <input  type="submit" name="logu"  id="logoutbn" value="log out" /></form>
+        <h4  id="status"> <span id="log"><?php   echo $_SESSION['username'];?></span> Logged In </h4>
+        <?php
+      if (isset ($_POST['logu']))
+
+      {
+
+             session_destroy();
+              header('location:index.php');
+
+
+      }
+
+
+
+    ?>
+        <h2 id="title">   <img  src="image/avatara.jpg.jpg" alt="test icon"  id="avator" >  Online Examination System -Admin dashboard </h2>
+
+
+
+
+
+
+
+
+        </header>
+
+
+        <div class="vnav">
+
+               <div class="sidebar">
+
+                    <h2> Navigator </h2>
+                    <ul>
+                        <li  ><a href ="adminhomepage.php" ><img  src="icons/test.ico" alt="test icon" class="icon" id="icon1"/>Tests</a></li>
+                        <li><a href ="adminstudentlist.php" id="onlink" ><img src="icons/student.ico" alt="student icon" class="icon" id="icon2"/>Students</a></li>
+                        <li><a href="adminfacultymanage.php"><img src="icons/faculty.ico" alt="faculty icon" class="icon" id="icon3"/>Faculty </a></li>
+                        <li><a href="admincourse.php"><img src="icons/feedback.ico" alt="feedback icon" class="icon" id="icon4"/>Add course</a></li>
+
+                    </ul>
+
+
+                </div>
+
+        </div>
+
+        <div class="mainc">
+
+
+            <div class="tabcon">
+                
+              <div class="table-responsive"> 
+               <h2> List of Students Registerd for Exam </h2>
+                <center>
+              
+                <table  id="editable_table" class="table table-bordered table-">
+               <thead>
+              <tr>
+                <th>ID</th>
+                <th>Full Name</th>
+                <th>GENDER</th>
+                <th>PHONE</th>
+                <th>USERNAME</th>
+                <th>PASSWORD</th>
+           
+
+
+              </tr>
+            </thead>
+            <tbody>
+   <?php
+              $query= ' select id , fullname, gender,phone, username,password from student';
+                $result= $conn->query($query);
+
+                if ($result->num_rows >0)
+                {
+                  while ($row=$result->fetch_assoc()){
+                   echo"<tr><td>".$row["id"]."</td><td>".$row["fullname"]."</td><td>".$row["gender"]."</td><td>"
+                   .$row["phone"]."</td><td>".$row["username"]."</td><td>".$row["password"]."</td></tr>";
+                  }
+                }
+
+                ?>        
+            </tbody>
+
+            </table>
+
+                </center>
+              </div>
+
+            </div>
+
+
+
+
+
+
+                </div>
+
+
+
+
+<script>  
+$(document).ready(function(){  
+     $('#editable_table').Tabledit({
+      url:'action.php',
+      columns:{
+       identifier:[0, "id"],
+       editable:[[1, 'fullname'], [2, 'gender'], [3, 'phone'], [4, 'username'],[5, 'password']]
+      },
+      restoreButton:false,
+      onSuccess:function(data, textStatus, jqXHR)
+      {
+       if(data.action == 'delete')
+       {
+        $('#'+data.id).remove();
+       }
+      }
+     });
+ 
+});  
+ </script>
+
+
+
+
+
+        <footer>
+        <footer>
+        <h2  id="copy">&copy; Copyright 2019
+                    </h2></footer>
+
+
+        </footer>
+
+
+    </body>
+
+
+
+
+</html>
